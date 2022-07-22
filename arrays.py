265d37816ea7da0c651520c4ccb43b47ad8e44e5
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-07-22 11:43:03 +0300

    Create arrays.py
    Kodlama.io

---
+++ arrays.py
@@ -0,0 +1,28 @@
+# Dizi Temelli Yapilar - Eyyub Eren
+
+# ----------------------- Bolum Odevi 1
+print("\nBolum Odevi 1")
+
+students = ["Ali","Ayse","Eyup","Fatma"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+
+students[2] = "Tembel Eyup"
+print(students[2])
+print(students[3])
+
+arrayLength = len(students)
+print("Siniftaki ogrenci sayisi: " + str(arrayLength))
+
+# Cevap 1: Evet :)
+# Cevap 2: Sanirim :)
+
+
+# ----------------------- Bolum Odevi 2
+
+# Hepsiburada sitesinde array olarak gorev yapan kisimlar:
+# Urunlerin kategorilerine girildiginde sol tarafta bulunan 
+# filtreleme secenekleri ve urun turleri (alt dallariyla)
+# dizi yapilarina orneklerdendir.
