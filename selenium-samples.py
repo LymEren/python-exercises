1ee3d92c2cba1550fa05dfd8d6d289b5273dd8aa
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-08-25 15:35:15 +0300

    Create selenium-samples.py

---
+++ selenium-samples.py
@@ -0,0 +1,17 @@
+from seleniun import webdriver
+import time
+browser = webdriver.Chrome()
+
+
+browser.get("https://www.google.com")
+
+# Im feeling lucky page
+lucky_page = browser.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[2]")  
+
+time.sleep(5)
+
+lucky_page.click()
+
+time.sleep(5)
+
+browser.close()
