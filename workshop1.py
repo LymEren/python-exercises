efbfcf2db19141e4a1c3cb77c2d4df87e1991da4
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-10-11 01:00:26 +0300

    Update workshop1.py

--- workshop1.py
+++ workshop1.py
@@ -66,3 +66,40 @@ while tryCounter:
         
         else:
             print(f"\n{resultNum} sayisi bir asal sayidir.")
+            
+            
+            
+            
+## String Divider
+
+myNum = int(input("\nLutfen pozitif bir tam sayi giriniz: "))
+resultNum = myNum
+primeCounter = 0
+
+tryCounter = True;
+while tryCounter:
+    if myNum <= 0:
+        myNum = int(input("\nYanlis bir sayi girdiniz. Lutfen pozitif tam sayi giriniz: "))
+        resultNum = myNum
+    else:
+        tryCounter = False
+        while myNum % 2 == 0 or myNum % 3 == 0 or myNum % 5 == 0:
+            if myNum % 2 == 0:
+                myNum = myNum / 2
+                primeCounter += 1
+            elif myNum % 3 == 0:
+                myNum = myNum / 3
+                primeCounter += 1
+            elif myNum % 5 == 0:
+                myNum = myNum / 5
+                primeCounter += 1
+
+        if myNum > 1:
+            primeCounter += 1
+
+        if resultNum == 1:
+            print("1 sayısı günümüzde ne asal ne de bileşik kabul edilir ve özel bir durumu vardır. ")
+        elif primeCounter > 1:
+            print(f"\n{resultNum} sayisi bir asal sayi degildir.")
+        else:
+            print(f"\n{resultNum} sayisi bir asal sayidir.")
