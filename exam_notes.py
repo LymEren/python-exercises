18a117ffab15ee26abe8c064034fdd4c917d8be5
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-11-09 01:17:00 +0300

    Update exam_notes.py

--- exam_notes.py
+++ exam_notes.py
@@ -1,30 +1,44 @@
-getInfo = open("studentInfos.txt", "r")
+# Calculates the results by taking student information from text file and exam grades from another text file.
+
+getInfo = open("studentInfo.txt", "r")
 getNote = open("studentsNotes.txt", "r")
 
 with open(r"studentsNotes.txt", 'r') as ln:
     lineCount = len(ln.readlines())
 
+
 def std_result():
-    print("Ogrenci Adi: {}, Sinav sonucu: {}, Harf Notu: {}".format(stdInfo,stdNote,stdResult))
+    result = ("Students Name: {}, Exam Result: {}, Grade: {}\n".format(stdInfo, stdNote, stdResult))
+    with open('studentsResult.txt', 'a') as studentWriter:
+        studentWriter.write(result)
+
+
 def std_error():
-    print("Ogrenci Adi: {}, Lutfen ogrencinin notunu kontrol ediniz. (0 ile 100 arasi olmalidir)".format(stdInfo))
+    result = ("Students Name: {}, Please check the student's grade. (It must be between 0 and 100)\n".format(stdInfo))
+    with open('studentsResult.txt', 'a') as studentWriter:
+        studentWriter.write(result)
 
 for _ in range(lineCount):
     stdInfo = getInfo.readline().strip()
     stdNote = getNote.readline().strip()
 
-    if stdNote.isnumeric() == True:
+    if stdNote.isnumeric():
         stdResult = int(float(stdNote))
-        if stdResult >= 0 and stdResult <= 39:
-            stdResult = "E"; std_result()
-        elif stdResult >= 40 and stdResult <= 54:
-            stdResult = "D"; std_result()
-        elif stdResult >= 55 and stdResult <= 69:
-            stdResult = "C"; std_result()
-        elif stdResult >= 70 and stdResult <= 84:
-            stdResult = "B"; std_result()
-        elif stdResult >= 85 and stdResult <= 100:
-            stdResult = "A"; std_result()
+        if 0 <= stdResult <= 39:
+            stdResult = "E"
+            std_result()
+        elif 0 <= stdResult <= 54:
+            stdResult = "D"
+            std_result()
+        elif 55 <= stdResult <= 69:
+            stdResult = "C"
+            std_result()
+        elif 70 <= stdResult <= 84:
+            stdResult = "B"
+            std_result()
+        elif 85 <= stdResult <= 100:
+            stdResult = "A"
+            std_result()
         else:
             std_error()
 
@@ -34,4 +48,3 @@ for _ in range(lineCount):
             std_result()
         else:
             std_error()
-
