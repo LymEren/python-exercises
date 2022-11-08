dbd4718796289f88af9945c04d72838e8fc9c44d
Eyyub Eren <102672506+LymEren@users.noreply.github.com>
2022-11-09 01:12:45 +0300

    Update pingLogger.py

--- pingLogger.py
+++ pingLogger.py
@@ -1,35 +1,30 @@
 # The program pinging in pingHost.txt file
 
-import os
+import requests
 from datetime import datetime
 
 pingList = {
-    "URL"      : [],
-    "Response" : [],
-    "Result"   : []
+    "URL": [],
+    "Response": [],
+    "Result": []
 }
 
-urlCounter = int(input("Kac tane URL pinglemek istiyorsunuz?: "))
+urlCounter = int(input("How many URLs do you want to ping?: "))
 
 for value in range(urlCounter):
-    pingInput = input(f"Pinglemek istediginiz {value + 1}. URL'i giriniz: ")
-    pingList["URL"].append(pingInput)
+    pingInput = input(f"Enter the {value + 1}. URL you want to ping: ")
+    pingList["URL"].append("http://" + pingInput)
 
 for value in range(urlCounter):
-    if (os.system("ping " + pingList["URL"][value])) == 0:
-        pingList["Response"].append("200")
+    if requests.get(pingList["URL"][value]).status_code == 200:
+        pingList["Response"].append(requests.get(pingList["URL"][value]).status_code)
         pingList["Result"].append("UP")
-    else:
-        pingList["Response"].append("500")
+    elif requests.get(pingList["URL"][value]).status_code == 500:
+        pingList["Response"].append(requests.get(pingList["URL"][value]).status_code)
         pingList["Result"].append("DOWN")
 
     with open('pingHost.txt', 'a') as pLog:
-        pLog.write("{} - URL: {}, Response: {}, Website is {}\n".format(datetime.now(), pingList["URL"][value], pingList["Response"][value], pingList["Result"][value]))
-
-
-print(pingList)
-
-
-
-
-
+        pLog.write("{} - URL: {}, Response: {}, Website is {}\n".format(datetime.now(),
+                                                                        pingList["URL"][value],
+                                                                        pingList["Response"][value],
+                                                                        pingList["Result"][value]))
